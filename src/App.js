890c998dbcb7aeff00ee3ef2a8797fb26a5644bc
dev-yihyun <dev_yihyun@naver.com>
2025-02-09 01:36:51 +0900

    오류 수정

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
-import AboutMe from "./component/AboutMeComponent";
-import Contact from "./component/ContactComponent";
-import Footer from "./component/FooterComponent";
-import Index from "./component/IndexComponent";
-import Profile from "./component/ProfileComponent";
-import ProjectMain from "./component/ProjectMainComponent";
-import ProjectToy from "./component/ProjectToyComponent";
-import Root from "./component/Root";
-import Skill from "./component/SkillComponent";
-import ScrollFadeBlock from "./ui/ScrollFade";
+import "./assets/font.css";
+import "./assets/root.css";
+import Home from "./page/Home";
 
 function App() {
-  return (
-    <>
-      <Root/>
-      <Index/>
-      <ScrollFadeBlock scrollfade="SkillFade">
-        <Skill/>
-      </ScrollFadeBlock>
-      <ProjectMain/>
-      <ScrollFadeBlock scrollfade="ToyFade">
-        <ProjectToy/>
-      </ScrollFadeBlock>
-      <AboutMe/>
-      <Profile/>
-      <ScrollFadeBlock scrollfade="ContactFade">
-        <Contact/>
-      </ScrollFadeBlock>
-      <Footer/>
-    </>
-  );
+    console.log("## ver.2 ##");
+    return (
+        <>
+            <Home />
+        </>
+    );
 }
 
 export default App;
