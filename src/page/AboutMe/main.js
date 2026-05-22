9e0447bcf010361422687b018a0d196ea4ed0abc
dev-yihyun <dev_yihyun@naver.com>
2026-05-22 23:20:54 +0900

    feat: AboutMe 문구 순서 변경

--- src/page/AboutMe/main.js
+++ src/page/AboutMe/main.js
@@ -18,8 +18,12 @@ function AboutMe() {
                 <ScrollFadeBlock scrollfade="AboutRightFade">
                     <div className="aboutme-right">
                         <li>
-                            <strong>사용자의 중심 사고</strong>로 생각하며 일의 즐거움을 느낍니다.
-                            <br />
+                            <strong>사용하기 쉬운 UI</strong>와 <strong>기억에 남는 UX</strong>를
+                            만드는 즐거움을 느낍니다.
+                        </li>
+                        <li>
+                            개발은 혼자가 아닌
+                            <strong> 함께 만드는 과정</strong>이라고 생각합니다.
                         </li>
                         <li>
                             <strong>문제를 분석</strong>하고 <strong>해결법을 찾는것</strong>에
@@ -32,14 +36,6 @@ function AboutMe() {
                         </li>
                         <li>
                             <strong>코드의 재사용성</strong>을 높이는 것을 좋아합니다.
-                        </li>
-                        <li>
-                            개발은 혼자가 아닌
-                            <strong> 함께 만드는 과정</strong>이라고 생각합니다.
-                        </li>
-                        <li>
-                            <strong>사용하기 쉬운 UI</strong>와 <strong>기억에 남는 UX</strong>를
-                            만드는 프론트엔드 개발자 입니다.
                         </li>
                     </div>
                 </ScrollFadeBlock>
