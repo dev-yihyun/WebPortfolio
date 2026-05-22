4c98ceba35d507c7f72650e303a27aa431bf8b9c
dev-yihyun <dev_yihyun@naver.com>
2026-05-22 23:08:58 +0900

    feat: Index 소개 문구 수정

--- src/page/Index/main.js
+++ src/page/Index/main.js
@@ -13,10 +13,10 @@ function Index() {
                     <p className="IndexTitle2 font1">YI-HYUN</p>
                     <div className="IndexText font2">
                         <div>
-                            끊임없이 <strong>성장하기 위해 노력</strong>합니다.
+                            <strong>기술과 경험</strong>을<strong>기록</strong>하며
                         </div>
                         <div>
-                            <strong>소통을 중요시하는 </strong> 개발자 입니다.
+                            <strong>더 나은 개발자</strong>가 되기 위해 성장하고 있습니다.
                         </div>
                     </div>
                 </div>
